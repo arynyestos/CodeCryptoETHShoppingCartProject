4b21f3990fe118c8856f4a4f89448945f736d591
arynyestos <arynyestos@gmail.com>
2023-12-26 17:31:43 +0100

    Upload ETH shopping cart project

---
+++ front/src/Cesta.jsx
@@ -0,0 +1,80 @@
+import { useContext, useEffect, useState } from 'react'
+import { Contexto } from './main'
+import { Link } from 'react-router-dom'
+import { ethers } from 'ethers'
+
+export function Cesta() {
+
+    const [estado, setEstado] = useContext(Contexto)
+    const total = estado.cesta.reduce((acum, item) => acum + item.total, 0)
+    const [cuenta, setCuenta] = useState()
+    const [txOk, setTxOk] = useState()
+    const [txRechazo, setTxRechazo] = useState()
+
+    useEffect(() => {
+        ethereum && ethereum.request({ method: 'eth_requestAccounts' }).then(cuentas => {
+            setCuenta(cuentas[0])
+            ethereum.on('accountsChanged', (cuentas) => {
+                setCuenta(cuentas[0])
+            })
+        })
+    }, [])
+
+    async function pagar() {
+        console.log("Inicio pagar...")
+        setTxOk(false)
+        setTxRechazo(false)
+        const paramsTx = {
+            to: '0x31e0FacEa072EE621f22971DF5bAE3a1317E41A4', // Cuenta del comercio
+            from: ethereum.selectedAddress, // La cuenta activa en Metamask
+            value: ethers.utils.parseEther(total.toString()).toHexString()
+        }
+        try {
+            console.log(`Intento enviar tx con ${total} ETH...`)
+            const txHash = await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [paramsTx],
+            })
+            setTxOk(txHash) // Transacción enviada
+        } catch (error) {
+            console.log(error)
+            setTxRechazo(error) // Transacción cancelada por el motivo que sea
+        } finally {
+            // FInal
+        }
+    }
+
+    return (
+        <div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {estado.cesta.map(item => (
+                        <tr key={item.producto.ProductID}>
+                            <td>
+                                <Link to={`/productos/${item.producto.ProductID}`}>
+                                    {item.producto.ProductName}
+                                </Link>
+                            </td>
+                            <td>{item.producto.UnitPrice}</td>
+                            <td>{item.cantidad}</td>
+                            <td>{item.total}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div>Total a pagar: {total}</div>
+            <div>Cuenta: {cuenta}</div>
+            <button className='my-3 btn btn-primary' onClick={pagar}>Pagar</button>
+            {txOk && <div className=' my-3 alert alert-success'>Transacción enviada: {txOk}</div>}
+            {txRechazo && <div className=' my-3 alert alert-danger'>Transacción cancelada: {JSON.stringify(txRechazo.message)}</div>}
+        </div>
+    )
+}
